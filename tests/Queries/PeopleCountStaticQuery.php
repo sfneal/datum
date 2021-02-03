4d95d8e72489c1324883bab5d56ca0797485801b
Stephen Neal <stephen.neal14@gmail.com>
2021-02-03 14:14:09 -0500

    MAKE QueryStaticTest

---
+++ tests/Queries/PeopleCountStaticQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Sfneal\Datum\Tests\Queries;
+
+
+use Sfneal\Datum\Tests\Models\People;
+use Sfneal\Queries\AbstractQueryStatic;
+
+class PeopleCountStaticQuery extends AbstractQueryStatic
+{
+    /**
+     * Execute a static query to retrieve the number of People records
+     *
+     * @return int
+     */
+    public static function execute(): int
+    {
+        return People::query()->count();
+    }
+}
---
+++ tests/QueryStaticTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sfneal\Datum\Tests;
+
+use Sfneal\Datum\Tests\Queries\PeopleCountStaticQuery;
+
+class QueryStaticTest extends TestCase
+{
+    public function test_execute_method()
+    {
+        $this->assertTrue(method_exists(PeopleCountStaticQuery::class, 'execute'));
+    }
+
+    public function test_static_query_results()
+    {
+        $count = PeopleCountStaticQuery::execute();
+
+        $this->assertIsInt($count);
+        $this->assertSame(23, $count);
+    }
+}
