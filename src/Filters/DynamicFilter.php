f4a55a903800eda9bf22bdde0da08616091851ad
Stephen Neal <stephen.neal14@gmail.com>
2021-02-15 13:56:31 -0500

    REFACTOR FilterDynamic to DynamicFilter

--- src/Filters/DynamicFilter.php
+++ src/Filters/DynamicFilter.php
@@ -5,7 +5,7 @@ namespace Sfneal\Filters;
 use Illuminate\Database\Eloquent\Builder;
 use Sfneal\Helpers\Strings\StringHelpers;
 
-abstract class FilterDynamic implements Filter
+abstract class DynamicFilter implements Filter
 {
     /**
      * @var Builder
--- tests/Queries/PeopleQueryWithFilters.php
+++ tests/Queries/PeopleQueryWithFilters.php
@@ -3,7 +3,7 @@
 namespace Sfneal\Datum\Tests\Queries;
 
 use Sfneal\Datum\Tests\Filters\CityFilter;
-use Sfneal\Datum\Tests\Filters\NameFirstFilterDynamic;
+use Sfneal\Datum\Tests\Filters\NameFirstDynamicFilter;
 use Sfneal\Datum\Tests\Filters\NameLastFilter;
 use Sfneal\Datum\Tests\Queries\Traits\PeopleBuilder;
 use Sfneal\Queries\FilterableQuery;
@@ -22,7 +22,7 @@ class PeopleQueryWithFilters extends FilterableQuery
         return [
             'city' => CityFilter::class,
             'name_last' => NameLastFilter::class,
-            'name_first' => NameFirstFilterDynamic::class,
+            'name_first' => NameFirstDynamicFilter::class,
         ];
     }
 }
